c188f451a98e4ff39e50036a6c4542b2bf4cc2a1
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-07-21 21:43:13 +0200

    add: eventListener to see user choices in console

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-
-
+//VARIABILI
 const cards = [1,1,2,2,3,3,4,4,5,5,6,6];
 const gridGame = document.querySelector(".grid-game");
 const square = document.querySelector(".square");
 
+//START 
 shuffle(cards);
+
+
 
 //FUNZIONI
 
@@ -18,14 +20,18 @@ function createDiv(number) {
 
   //Carte random
   function shuffle(array) {
-
+    //ciclo nelle carte, genero un numero random (con indice) e sostituisco indice con numero random
     for (let i = array.length - 1; i > 0; i--) {
         let randomIndex = Math.floor(Math.random() * (i + 1));
         [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
     }
     
+    //creo tanti div quanto sono le cards con contenuto indice array random
     for (let i = 0; i < array.length; i++) {
         const newSquare = createDiv(array[i]);
+        newSquare.addEventListener('click', function() {
+            console.log(newSquare.innerHTML);
+        });
         gridGame.append(newSquare); 
     }
 }
